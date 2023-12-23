f3afa61de8881fcb176cce200beaa57b77ef1fde
Patrick Kim <patrick.yrkim@gmail.com>
2023-12-22 22:45:53 -0800

    Remove excess user info

--- teatime/admin.py
+++ teatime/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 
 # Register your models here.
 
 
 # Unregister Groups
 admin.site.unregister(Group)
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+
+    # display username fields on admin page
+    fields = ["username"]
+
+# Unregister initial user
+admin.site.unregister(User)
+
+# Reregister User
+admin.site.register(User, UserAdmin)
