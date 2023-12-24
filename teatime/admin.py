297643d5bea72872aca7fa9e5c3333af16c30911
Patrick Kim <patrick.yrkim@gmail.com>
2023-12-23 22:38:20 -0800

    Add profile inline to users

--- teatime/admin.py
+++ teatime/admin.py
@@ -8,16 +8,21 @@ from .models import Profile
 # Unregister Groups
 admin.site.unregister(Group)
 
+# Mix Profile info into User info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
 # Extend User Model
 class UserAdmin(admin.ModelAdmin):
     model = User
 
     # display username fields on admin page
     fields = ["username"]
+    inlines = [ProfileInline]
 
 # Unregister initial user
 admin.site.unregister(User)
 
 # Reregister User
 admin.site.register(User, UserAdmin)
-admin.site.register(Profile)
+# admin.site.register(Profile)
