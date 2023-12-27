d1ebcb042f5251428a7e4eaf57b78d678b8d044f
Patrick Kim <patrick.yrkim@gmail.com>
2023-12-26 23:48:02 -0800

    Update user follow themselves

--- teatime/models.py
+++ teatime/models.py
@@ -16,9 +16,13 @@ class Profile(models.Model):
         return self.user.username
 
 # Create Profile when new user signs up
+# @receiver(post_save, sender=User)  #same as line 28
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
+        user_profile.save()
+        # have user follow themselves
+        user_profile.follows.set([instance.profile.id])
         user_profile.save()
 
 post_save.connect(create_profile, sender=User)
