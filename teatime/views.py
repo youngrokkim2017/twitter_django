37fa9ee4424769a93915c6c71571f8acf65e6bca
Patrick Kim <patrick.yrkim@gmail.com>
2023-12-27 23:26:53 -0800

    Add profile list

--- teatime/views.py
+++ teatime/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def profile_list(request):
+    return render(request, 'profile_list.html')
