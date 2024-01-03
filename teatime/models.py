93d99bee933bafdb68006eca2af7b1e80bd0d3ce
Patrick Kim <patrick.yrkim@gmail.com>
2024-01-02 22:07:31 -0800

    Add tea model

--- teatime/models.py
+++ teatime/models.py
@@ -28,3 +28,20 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile.save()
 
 post_save.connect(create_profile, sender=User)
+
+# Create "tea" model (tweet)
+class Tea(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name="tea",
+        on_delete=models.DO_NOTHING
+    )
+    body = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return(
+            f"{self.user}"
+            f"({self.created_at: %Y-%m-%d %H:%M}): "
+            f"{self.body}..."
+        )
