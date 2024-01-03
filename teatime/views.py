e7185017adb96cf3ffb0ac4757485fa20773a433
Patrick Kim <patrick.yrkim@gmail.com>
2024-01-02 22:27:27 -0800

    Add tea and update styling

--- teatime/views.py
+++ teatime/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
-from .models import Profile
+from .models import Profile, Tea
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    if request.user.is_authenticated:
+        teas = Tea.objects.all().order_by("-created_at")
+
+
+    return render(request, 'home.html', {"teas": teas})
 
 def profile_list(request):
     if request.user.is_authenticated:
@@ -18,6 +22,7 @@ def profile_list(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+        teas = Tea.objects.filter(user_id=pk).order_by("-created_at")
 
         # POST form logic
         if request.method == "POST":
@@ -33,7 +38,7 @@ def profile(request, pk):
             # save profile
             current_user_profile.save()
 
-        return render(request, "profile.html", {"profile": profile})
+        return render(request, "profile.html", {"profile": profile, "teas": teas})
     else:
         messages.success(request, ("You must be logged in to view this page..."))
         return redirect('home')
