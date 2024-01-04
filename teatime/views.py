64c3a74ce3d5eabfa3e4516d3375263bd8bd328f
Patrick Kim <patrick.yrkim@gmail.com>
2024-01-04 01:19:08 -0800

    Update login logic

--- teatime/views.py
+++ teatime/views.py
@@ -57,4 +57,17 @@ def profile(request, pk):
         return redirect('home')
 
 def login_user(request):
-    return render(request, "login.html", {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in!"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error logging in. Please try again."))
+            return redirect('login')
+    else:
+        return render(request, "login.html", {})
