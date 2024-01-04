521d4de4684ae83bb241a48568bfb4c123544e78
Patrick Kim <patrick.yrkim@gmail.com>
2024-01-04 01:21:47 -0800

    Add logout

--- teatime/urls.py
+++ teatime/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('profile_list/', views.profile_list, name="profile_list"),
     path('profile/<int:pk>/', views.profile, name="profile"),
     path('login/', views.login_user, name="login"), 
+    path('logout/', views.logout_user, name="logout"), 
 ]
