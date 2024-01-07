3360fe414e9919103df4ad86ac762a7e29088ab8
Patrick Kim <patrick.yrkim@gmail.com>
2024-01-06 22:29:02 -0800

    Add SignUpForm to register user views

--- teatime/views.py
+++ teatime/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django import forms 
 
 from .models import Profile, Tea
-from .forms import TeaForm
+from .forms import TeaForm, SignUpForm
 
 # Create your views here.
 def home(request):
@@ -78,4 +80,21 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, "register.html", {})
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # first_name = form.cleaned_data['first_name']
+            # last_name = form.cleaned_data['last_name']
+            # email = form.cleaned_data['email']
+
+            # log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have successfully registered!"))
+            return redirect('home')
+    
+    return render(request, "register.html", {'form': form})
