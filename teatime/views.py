546617c9bbfeb0447221f2f8a2153ba696c04976
Patrick Kim <patrick.yrkim@gmail.com>
2024-01-09 17:38:33 -0800

    Update form logic

--- teatime/views.py
+++ teatime/views.py
@@ -6,7 +6,7 @@ from django import forms
 from django.contrib.auth.models import User
 
 from .models import Profile, Tea
-from .forms import TeaForm, SignUpForm
+from .forms import TeaForm, SignUpForm, ProfilePicForm
 
 # Create your views here.
 def home(request):
@@ -103,14 +103,20 @@ def register_user(request):
 def update_user(request):
     if request.user.is_authenticated:
         current_user = User.objects.get(id=request.user.id)
-        form = SignUpForm(request.POST or None, instance=current_user)
-        if form.is_valid():
-            form.save()
+        profile_user = Profile.objects.get(user__id=request.user.id)
+
+        # get forms
+        user_form = SignUpForm(request.POST or None, request.FILES or None, instance=current_user)
+        profile_form = ProfilePicForm(request.POST or None, request.FILES or None, instance=profile_user)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
             # django logs you out after update, re login
             login(request, current_user)
             messages.success(request, ("Your profile has been updated!"))
             return redirect('home')
-        return render(request, "update_user.html", {'form': form  })
+        return render(request, "update_user.html", {'user_form': user_form, 'profile_form': profile_form })
     else:
         messages.success(request, ("You must be logged in to view that page"))
         return redirect('home')
