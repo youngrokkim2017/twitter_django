9d784110bbc48b1c14583ecba772aeef81631293
Patrick Kim <patrick.yrkim@gmail.com>
2024-01-16 22:50:39 -0800

    Update profile form

--- teatime/forms.py
+++ teatime/forms.py
@@ -46,6 +46,19 @@ class SignUpForm(UserCreationForm):
 class ProfilePicForm(forms.ModelForm):
     profile_image = forms.ImageField(label="Profile Picture")
 
+    profile_bio = forms.CharField(label="Profile Bio", widget=forms.Textarea(attrs={'class':'form-control', 'placeholder':'Profile Bio'}))
+    homepage_link = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Website Link'}))
+    facebook_link = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Facebook Link'}))
+    instagram_link = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Instagram Link'}))
+    linkedin_link = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Linkedin Link'}))
+
     class Meta:
         model = Profile
-        fields = ('profile_image', )
+        fields = (
+            'profile_image', 
+            'profile_bio',
+            'homepage_link',
+            'facebook_link',
+            'instagram_link',
+            'linkedin_link', 
+        )
